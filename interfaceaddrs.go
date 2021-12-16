ddd80e4d6e9ec17f479ddc71dc7cf373dc994a93
mcrakhman <rakhmanov.m@gmail.com>
2021-12-16 10:21:30 +0100

    Add empty method to other archs

--- interfaceaddrs.go
+++ interfaceaddrs.go
@@ -5,6 +5,17 @@ package multiaddr
 
 import "net"
 
+func SetInterfaceAddrsGetter(getter InterfaceAddrsGetter) {}
+
+type InterfaceAddr struct {
+	Ip     []byte
+	Prefix int
+}
+
+type InterfaceAddrsGetter interface {
+	InterfaceAddrs() []InterfaceAddr
+}
+
 func InterfaceAddrs() (addrs []net.Addr, err error) {
 	return net.InterfaceAddrs()
 }
